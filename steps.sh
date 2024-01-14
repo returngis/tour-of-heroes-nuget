54039418c31179d7615f68f2a407d030c7830f0b
0GiS0 <giselatb@outlook.com>
2024-01-14 20:49:51 +0000

    Update version and push new NuGet package

--- steps.sh
+++ steps.sh
@@ -4,6 +4,6 @@ dotnet build
 
 dotnet pack
 
-dotnet nuget push ./bin/Release/tour-of-heroes-nuget.1.0.0.nupkg --source github
+dotnet nuget push ./bin/Release/tour-of-heroes-nuget.1.0.3.nupkg --source github
 
 dotnet nuget push ./bin/Release/tour-of-heroes-nuget.1.0.0.nupkg --source github --api-key $PAT
