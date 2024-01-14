c4c7b529b49ba1267020ca6cc029ea5002fff60f
0gis0 <giselatb@outlook.com>
2024-01-14 14:48:19 +0000

    Update Nexus API key and source URL

--- nexus-steps.sh
+++ nexus-steps.sh
@@ -10,10 +10,12 @@ docker exec nexus cat /nexus-data/admin.password
 
 # Go to http://localhost:8081/#user/nugetapitoken
 
-$API_KEY="f2e96588-8865-36ab-978e-8908df69c11a"
+API_KEY="310ddc61-f0c2-3e71-863e-3312101eb39d"
+
+# Security -> Realms -> NuGet API Key Realm -> Enabled
 
 dotnet build
 
 dotnet pack
 
-dotnet nuget push ./bin/Release/tour-of-heroes-nuget.1.0.0.nupkg --source http://localhost:8081/repository/nuget-hosted/ --api-key $API_KEY
+dotnet nuget push ./bin/Release/tour-of-heroes-nuget.1.0.0.nupkg --source http://nexus:8081/repository/nuget-hosted/ --api-key $API_KEY
