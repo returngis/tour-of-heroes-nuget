20fd7aa7ac1fd829690791a772857d2dc06bf773
0GiS0 <giselatb@outlook.com>
2024-01-14 12:57:14 +0000

    Update devcontainer.json and docker-compose.yml, add nexus-steps.sh and nuget.config

---
+++ nexus-steps.sh
@@ -0,0 +1,27 @@
+# Using Docker Desktop exec this in the nuget container: cat /nexus-data/admin.password
+
+# Go to http://localhost:8081/#user/nugetapitoken
+
+API_KEY="dfc7f9c2-264d-3ff9-8ec1-16e6a70e1aea"
+
+cat <<EOF > nuget.config
+<?xml version="1.0" encoding="utf-8"?>
+<configuration>
+    <packageSources>
+        <clear />
+        <add key="nexus" value="http://localhost:8081/repository/nuget-hosted/" />
+    </packageSources>
+    <packageSourceCredentials>
+        <nexus>
+            <add key="Username" value="admin" />
+            <add key="ClearTextPassword" value="$API_KEY" />
+        </nexus>
+    </packageSourceCredentials>
+</configuration>
+EOF
+
+dotnet build
+
+dotnet pack
+
+dotnet nuget push ./bin/Release/tour-of-heroes-nuget.1.0.0.nupkg --api-key $API_KEY --source http://localhost:8081/repository/nuget-hosted/
