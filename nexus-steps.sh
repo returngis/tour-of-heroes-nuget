1b8d6a059429d1a9ef9bf916f6a75d91c039ba0f
0gis0 <giselatb@outlook.com>
2024-01-14 14:48:57 +0000

    Update nexus-steps.sh script

--- nexus-steps.sh
+++ nexus-steps.sh
@@ -1,12 +1,5 @@
-docker volume create --name nexus-data
-
-docker run -d -p 8081:8081 --name nexus -v nexus-data:/nexus-data sonatype/nexus3
-
-# Wait for nexus to start
-sleep 60
-
 # Get the admin password
-docker exec nexus cat /nexus-data/admin.password
+# cat /nexus-data/admin.password
 
 # Go to http://localhost:8081/#user/nugetapitoken
 
@@ -19,3 +12,5 @@ dotnet build
 dotnet pack
 
 dotnet nuget push ./bin/Release/tour-of-heroes-nuget.1.0.0.nupkg --source http://nexus:8081/repository/nuget-hosted/ --api-key $API_KEY
+
+
